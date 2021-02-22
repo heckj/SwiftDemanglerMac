fe3ab19c33f08e779f5c6804004e8ce5d3e88967
Joe Heck <heckj@mac.com>
2021-02-22 10:35:42 -0800

    Initial Commit

---
+++ SwiftDemanglerMac/SwiftDemanglerMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemanglerMacApp.swift
+//  SwiftDemanglerMac
+//
+//  Created by Joseph Heck on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemanglerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
